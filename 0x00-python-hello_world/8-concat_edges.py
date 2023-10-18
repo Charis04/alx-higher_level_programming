2c80480d258b304d0207d671945c651a7025a340
Charis <charisadu@gmail.com>
2023-10-18 00:13:38 +0000

    concat edges

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[str.find("object"):str.find("object") + 27] + " " + str[str.find("wi"):str.find("wi") + 4] + " " + str[str.find("Py"):str.find("Py") + 6]
+print(str)
