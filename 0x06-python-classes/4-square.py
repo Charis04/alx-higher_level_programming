a0db3341671b5fce711a22712754718d73b8d095
Charis <charisadu@gmail.com>
2023-10-24 23:01:36 +0000

    not important

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,7 +12,7 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
@@ -25,7 +25,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Return the current area of the square."""
--- 0x06-python-classes/push.sh
+++ 0x06-python-classes/push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod 777 *
 git add .
 git commit -m "not important"
 git push
