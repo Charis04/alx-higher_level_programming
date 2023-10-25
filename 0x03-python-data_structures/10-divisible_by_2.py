30c7343f97e76e5f6ba22b6fa3d87ece75abf874
Charis <charisadu@gmail.com>
2023-10-25 17:30:32 +0000

    not important

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    atarashi_list = []
+    if my_list:
+        for num in my_list:
+            atarashi.append(False if num % 2 else True)
+        return atarashi_list
+
