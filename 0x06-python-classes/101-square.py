c667f77cda3f2d0d63e66b0e133752e918ecd2a1
Charis <charisadu@gmail.com>
2023-10-25 00:32:57 +0000

    not important

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -53,7 +53,7 @@ class Square:
             return
 
         [print("") for i in range(0, self.__position[1])]
-        for a in range(0, self.__size):
+        for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
