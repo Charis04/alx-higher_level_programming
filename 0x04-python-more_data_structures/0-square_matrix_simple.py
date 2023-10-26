c5a6b885faf886bb8f7eef5f0f5a25f10733190d
Charis <charisadu@gmail.com>
2023-10-26 13:22:15 +0000

    not important

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     atarashi_matrix = matrix.copy()
 
     for x in range(len(matrix)):
-        atarashi_matrix = list(map(lambda x: x**2, matrix[x]))
+        atarashi_matrix[x] = list(map(lambda x: x**2, matrix[x]))
 
     return (atarashi_matrix)
