1e4723c31b3990c83b2492adeafd303f3d9da2ed
Charis <charisadu@gmail.com>
2023-11-17 13:21:03 +0000

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+    if number < 0:
+        mod = number * -1
+    else:
+        mod = number
+
+    while mod >= 10:
+        mod %= 10
+    print(mod, end="")
