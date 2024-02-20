fc0585bd9e1c0b6c7763146f2c2e1cb8eee17aab
Charis Adu <charisadu@gmail.com>
2024-02-20 04:39:22 +0100

    create lookup function to get list of all attributes and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+def lookup(obj):
+    """Gets all available attributes and methods of an object.
+    Args:
+    obj (obj): The object whose attributes and methods are to be gotten.
+    """
+    return dir(obj)
