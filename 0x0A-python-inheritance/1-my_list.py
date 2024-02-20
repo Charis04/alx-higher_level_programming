9ca91b192e4e2c0f49f21f42164bcb6d93f76af3
Charis <charisadu@gmail.com>
2024-02-20 13:56:51 +0000

    more tests

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,6 @@
 class MyList(list):
     """Represents a list"""
 
-    def __init__(self):
-        super().__init__()
-
     def print_sorted(self):
         """Prints a sorted list"""
         print(sorted(self))
