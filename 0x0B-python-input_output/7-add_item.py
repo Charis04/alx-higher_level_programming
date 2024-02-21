dc40337e9f27689dba92257428ed4d70ef64d2d9
Charis <charisadu@gmail.com>
2024-02-21 12:40:56 +0000

    create program that adds all arguements to a list and saves to a json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""A program that adds all arguements to a list and saves to a json file"""
+
+
+def main():
+    ls = get_arg_list(sys.argv)
+    filename = "add-item.json"
+    cont = load_from_json_file(filename)
+    cont += ls
+    save_to_json_file(cont, filename)
+
+
+def get_arg_list(l):
+    a = []
+    for i in l[1:]:
+        a.append(i)
+    return a
+
+
+if __name__ == "__main__":
+    main()
