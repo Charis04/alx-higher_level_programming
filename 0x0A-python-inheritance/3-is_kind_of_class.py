225a775afbc943b12c23277daff6d645bfbd7dc3
Charis <charisadu@gmail.com>
2024-02-22 10:34:10 +0000

    create fuction that checks if an obj is an instance of a class or sublcass of that class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if an object is an instance of a specified class or an instance
+    of a subclass of the specified class
+    Args:
+        obj (obj): object to check
+        a_class (a_class): the specified class to check against
+    Returns:
+        True if it is an instance and False if otherwise
+    """
+
+    return isinstance(obj a_class)
