089007e62a573c967fcd64703439f5de96177c99
Charis <charisadu@gmail.com>
2024-02-22 00:05:30 +0000

    create a class Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Program defines a class"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of the obj"""
+
+        return vars(self)
