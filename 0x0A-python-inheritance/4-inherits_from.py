41412766687f70315f05602cbc0bb686749af91c
Charis <charisadu@gmail.com>
2024-02-22 10:54:08 +0000

    create a fuction that checks for inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Program a fucntion"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object inherits from a specified class"""
+
+    return issubclass(obj, a_class)
