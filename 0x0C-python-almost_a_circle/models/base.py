f8b586b1043f96a676a688a45fe8e885dcb49951
Charis <charisadu@gmail.com>
2024-02-28 20:49:45 +0000

    document base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,6 +10,8 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initilises the class"""
+
         if id is not None:
             self.id = id
         else:
@@ -18,12 +20,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Converts a list of dictionaries to json string"""
+
         if not list_dictionaries or list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """Saves a list of objects to a file as json string"""
+
         f_name = f"{cls.__name__}.json"
         content = []
         if list_objs is not None:
@@ -36,18 +42,24 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """Converts a json string to a list of dictionaries"""
+
         if not json_string or json_string is None:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+        """Creates an instance of a class from a dictionary"""
+
         obj = cls(1, 1)
         obj.update(**dictionary)
         return obj
 
     @classmethod
     def load_from_file(cls):
+        """Loads a list of objects from a file of json string"""
+
         f_name = f"{cls.__name__}.json"
         try:
             with open(f_name, 'r', encoding='utf-8') as jf:
@@ -63,6 +75,8 @@ class Base:
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
+        """Saves a list of objects to a CSV file"""
+
         f_name = f"{cls.__name__}.csv"
         content = []
         if list_objs is not None:
@@ -74,6 +88,8 @@ class Base:
 
     @classmethod
     def load_from_file_csv(cls):
+        """Loads a list of objects from a CSV file"""
+
         f_name = f"{cls.__name__}.csv"
         try:
             df = pd.read_csv(f_name)
