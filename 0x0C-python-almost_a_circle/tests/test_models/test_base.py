22afded0e49d974614907dc58a471c1740eb66f8
Charis <charisadu@gmail.com>
2024-02-28 20:25:20 +0000

    write script that tests Base id

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Tests for Base class"""
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_id_exist(self):
+        b1 = Base()
+        self.assertTrue(b1.id)
+
+    def test_id_plus(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
