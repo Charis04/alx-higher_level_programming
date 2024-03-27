c1679c6fc52d1f484558368ecf516be4bc67dc79
Charis <charisadu@gmail.com>
2024-03-27 23:24:15 +0000

    list cities and state from join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- list all cities
-SELECT cities.id, cities.name states.name FROM cities
+SELECT cities.id, cities.name, states.name FROM cities
 	INNER JOIN states ON cities.state_id = states.id
 	ORDER BY cities.id ASC;
