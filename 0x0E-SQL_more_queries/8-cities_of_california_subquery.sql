aabdc5a095df83f1fb662a4e2c0694e2796b0ffa
Charis <charisadu@gmail.com>
2024-03-27 22:56:10 +0000

    list cities in carlifonia

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- list cities in carlifonia
 SELECT id, name FROM cities
-	WHERE state_id = (SELECT id FROM states WHERE name = 'carlifonia')
+	WHERE state_id = (SELECT id FROM states WHERE name = 'Carlifonia')
 	ORDER BY id ASC;
