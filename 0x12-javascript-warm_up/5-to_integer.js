f8fe31122824980811a6c80f60f71440f013b77e
Charis <charisadu@gmail.com>
2024-04-23 02:39:11 +0000

    write script that prints arg if its a num

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const firstArg = parseInt(process.argv[2]);
-if (typeof firstArg === "number" && !isNaN(firstArg)) {
+if (typeof firstArg === 'number' && !isNaN(firstArg)) {
   console.log('My number:', firstArg);
 } else {
   console.log('Not a number');
