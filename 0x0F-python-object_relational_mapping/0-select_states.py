05666252202b0d8fdbd530897352e36a13057324
Charis04 <charisadu@gmail.com>
2024-04-27 23:02:16 +0100

    use Mysqldb to select rows

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 
 import MySQLdb
+import sys
+
+def main():
+    db = MySQLdb.connect(host="mysql.server", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
