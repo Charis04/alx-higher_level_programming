e52d4efae6f050436f236d5c62a1f88b72c892f2
Charis <charisadu@gmail.com>
2024-05-03 16:51:57 +0100

    'query states for state ma tching 4th arg

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,8 @@ def main():
         host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4]))
+    qry = "SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4])
+    cur.execute(qry)
     rows = cur.fetchall()
     for row in rows:
         print(row)
