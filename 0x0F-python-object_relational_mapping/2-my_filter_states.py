634e3e9c24fa81e11733b819a39b6abde52d565b
Charis <charisadu@gmail.com>
2024-05-03 16:30:59 +0100

    query states for state matching 4th arg

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,11 +13,10 @@ def main():
         host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute(f"SELECT * FROM states WHERE name like '{sys.argv[4]}'")
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == sys.argv[4]:
-            print(row)
+        print(row)
 
 
 if __name__ == "__main__":
