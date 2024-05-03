da281ddd2d604b7e9f8630f776a040b8386aa843
Charis <charisadu@gmail.com>
2024-05-03 16:26:38 +0100

    query states for state matching 4th arg

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Query states for state mathcing 4th arguement
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+        )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+
+
+if __name__ == "__main__":
+    main()
