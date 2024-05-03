1f3acae16be752184c2d813bec8fa89ce15929b0
Charis <charisadu@gmail.com>
2024-05-03 03:01:14 +0100

    address case sensitivity

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ def main():
         host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
         )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE UPPER(name) LIKE 'N%'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
