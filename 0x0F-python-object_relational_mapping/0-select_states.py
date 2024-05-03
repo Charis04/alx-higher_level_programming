03f5be846a3cf45362928adbb0cba3d3e342b738
Charis <charisadu@gmail.com>
2024-05-03 02:58:05 +0100

    query db for states starting with N

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
+"""
+Query All states for database
+"""
+
 import MySQLdb
 import sys
 
+
 def main():
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+        )
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
