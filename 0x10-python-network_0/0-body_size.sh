24129b7a29df869277b1e672583df7e0cac4ddef
Charis <charisadu@gmail.com>
2024-05-10 05:52:34 +0100

    get size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 # Get size of content
-
-# Get the URL from the first argument
-url="$1"
-
-# Use curl to get the response header size
-header_size=$(curl -sI "$url" -w "%{size_header}\n" -o /dev/null)
-
-# Use curl to download the response body and get its size
-body_size=$(curl -s "$url" -o /dev/null | wc -c)
-
-# Calculate total response size
-total_size=$((header_size + body_size))
-
-# Display the total size in bytes
-echo "$total_size"
+echo"$(curl -s "$1" -o /dev/null | wc -c)"
