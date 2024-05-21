5e6f0dd501b5d7a58f8a7b8b694da473fd2c6201
Charis <charisadu@gmail.com>
2024-05-21 03:01:31 +0100

    get header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A program that gets a header from a url using requests
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    header = response.headers["X-Request-Id"]
+    print(header)
