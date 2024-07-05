4b1470ae830521d16db5864e4f231699f2e9f00d
Charis <charisadu@gmail.com>
2024-07-05 02:26:57 +0100

    write a script that lists all cities of a state from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities of a state from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    qry = "select name from cities where state_id = (select id from states "\
+        "where name = %s)"
+    cur.execute(qry, (sys.argv[4],))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
