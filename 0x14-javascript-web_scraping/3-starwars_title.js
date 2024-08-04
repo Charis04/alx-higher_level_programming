7d660b3fb6f4ba6ce114b0e86542356c13a90ee2
Charis <charisadu@gmail.com>
2024-08-04 01:54:39 +0100

    req from api

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,6 +7,8 @@ const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
   } else {
     console.log(JSON.parse(body)['title']);
   }
